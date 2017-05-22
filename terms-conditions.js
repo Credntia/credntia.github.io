8f2881953e9a6adf36f1da2493368c8c40d51a42
Cody Winton <cody@credntia.com>
2017-05-22 13:07:29 -0500

    Attempting to read terms of service from markdown file

---
+++ terms-conditions.js
@@ -0,0 +1,7 @@
+var marked = require('marked');
+var fs = require('fs');
+
+var readMe = fs.readFileSync('Terms.md', 'utf-8');
+var markdownReadMe = marked(readMe);
+
+fs.writeFileSync('Terms.html', markdownReadMe);
