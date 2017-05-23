cdbf3cd3b45bbdff87359d5c7ed2192c01c11eee
Cody Winton <cody@credntia.com>
2017-05-22 22:38:43 -0500

    Improved Indenting based on editorconfig

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-    // mobile detection
-    if ((navigator.userAgent.match(/iPhone|Android|iPod/i))) {
-        $('.app-download').attr('href', 'https://k29hq.app.goo.gl/qL6j');
-    };
+	// mobile detection
+	if ((navigator.userAgent.match(/iPhone|Android|iPod/i))) {
+		$('.app-download').attr('href', 'https://k29hq.app.goo.gl/qL6j');
+	};
 
 });
